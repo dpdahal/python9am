153753b32f811f608f318957f4ae5a18cef38e92
dpdahal <dp48dahal@gmail.com>
2021-07-21 09:23:00 +0545

    update

---
+++ introduction.py
@@ -0,0 +1,19 @@
+# print("Hello python")
+
+# print("hello")
+# mother's
+# print('mother\'s')
+# print("mother's")
+# print("hello", "python", 9, "Am", "Class")
+# print(1234)
+
+# fName = 'Sophia'
+# lName = 'abc'
+# age = 20
+# print(f"My name is: {fName} {lName} is {age}")
+# print("First Name: {} Last Name: {} Age:{}".format(fName, lName, age))
+# print(fName, lName, age)
+
+# print(fName + " " + lName + age)
+
+
